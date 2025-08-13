3fd1e471802cc902037fe8656013fab2d91c9dbb
Eongyu Park <peg5325@gmail.com>
2025-08-14 02:51:00 +0900

    #69 - 회원 엔티티 디자인 수정: 생성자 메소드 추가
    인증 정보가 없을 땐 직접 생성자, 수정자 정보를 입력하여 회원 등록을 할 수 있도록 생성자 메소드를 추가함

--- src/main/java/com/springboard/projectboard/domain/UserAccount.java
+++ src/main/java/com/springboard/projectboard/domain/UserAccount.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "email", unique = true),
         @Index(columnList = "createdAt"),
@@ -39,16 +39,24 @@ public class UserAccount extends AuditingFields {
 
     protected UserAccount() {}
 
-    private UserAccount(String userId, String userPassword, String email, String nickname, String memo) {
+    private UserAccount(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
         this.userId = userId;
         this.userPassword = userPassword;
         this.email = email;
         this.nickname = nickname;
         this.memo = memo;
+        this.createdBy = createdBy;
+        this.modifiedBy = createdBy;
     }
 
+    // 기존 사용 코드
     public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
-        return new UserAccount(userId, userPassword, email, nickname, memo);
+        return new UserAccount(userId, userPassword, email, nickname, memo, null);
+    }
+
+    // 최초 회원가입 시 사용할 코드
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo, String createdBy) {
+        return new UserAccount(userId, userPassword, email, nickname, memo, createdBy);
     }
 
     @Override
