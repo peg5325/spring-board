3671ad089687c9296ccd6a571da411d4a1b38308
Eongyu Park <peg5325@gmail.com>
2025-09-19 19:46:23 +0900

    #78 - contextLoads()의 DB 의존 제거
    현재 계속 이 테스트를 통과하지 못하는 중.
    테스트 겸 수정 적용 해보았다.

--- src/test/java/com/springboard/projectboard/SpringBoardApplicationTests.java
+++ src/test/java/com/springboard/projectboard/SpringBoardApplicationTests.java
@@ -3,7 +3,7 @@ package com.springboard.projectboard;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
 class SpringBoardApplicationTests {
 
 	@Test
